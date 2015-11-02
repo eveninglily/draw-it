b91684b29a19ef12b649590184a9574c7e4e8227
Evan McIntire <mcintire.evan@gmail.com>
2015-11-01 23:45:02 -0500

    Move JS from index.html to core.js
    Move all the code that was in index.html to core.js for organizational
    purposes

---
+++ core.js
@@ -0,0 +1,44 @@
+var down = false;
+var canvas = $('#layer0').get(0);
+var can = new DrawingCanvas(canvas);
+            
+var layers = [can];
+var currentLayer = 0;
+            
+function start(x, y) {
+	layers[currentLayer].beginStroke(x, y);
+    layers[currentLayer].doStrokes();
+}
+            
+function move(x, y) {
+	layers[currentLayer].strokes[0].addPoint(x, y);
+    layers[currentLayer].doStrokes();
+}
+            
+function end() {
+	layers[currentLayer].completeStroke(layers[currentLayer].strokes[0]);
+    addChange(layers[currentLayer].strokes.pop());
+    $('.selected').children(':nth-child(1)').html(layers[currentLayer].toImage());
+}
+
+$('#layers').on('touchstart', function (evt) {
+	start(evt.originalEvent.changedTouches[0].pageX, evt.originalEvent.changedTouches[0].pageY);
+    down = true;
+}).on('touchmove', function (evt) {
+	if(down){
+    	move(
+        	evt.originalEvent.touches[0].pageX,
+            evt.originalEvent.touches[0].pageY
+		);
+    }
+}).on('mousedown', function(e) {
+	down = true;
+    start(e.offsetX, e.offsetY);
+}).on('mouseup touchend', function(e) {
+	end();
+    down = false;
+}).on('mousemove', function(e) {
+	if(down) {
+    	move(e.offsetX, e.offsetY);
+	}
+});

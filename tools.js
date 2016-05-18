b07b57479a684a816d9f2fc5b7f32b95caaa6fc0
Evan McIntire <mcintire.evan@gmail.com>
2016-05-17 22:27:34 -0400

    Allow reordering of palette elements

--- tools.js
+++ tools.js
@@ -118,21 +118,91 @@ function loadPalette() {
     }
     reader.readAsText(file);
 }
-
+var lClick;
 function addPaletteItem(color) {
-    $('<div>')
+    var paletteItem = $('<div>')
         .addClass('color')
         .css({background: color})
-        .appendTo('#color-palette')
         .on('click', function(e) {
             var rgb = color;
             currTool.color = rgb;
+        }).on('mousedown', function(e) {
+            $('.currColor').removeClass('currColor');
+            $(this).addClass('currColor');
+            $(this).data("longClick", setTimeout(function(){
+			    lClick = true;
+		    },200));
         });
+
+        paletteItem.appendTo('#color-palette');
+        $(document).on('mouseup', function() {
+		    clearTimeout(paletteItem.data("longClick"));
+		    lClick = false;
+	    });
 }
+
+//TODO: write common function for this type of thing
+$('#color-settings').on('mousemove', function(e) {
+    if(lClick) {
+        var r = $('.currColor');
+
+        e.preventDefault();
+		document.getSelection().removeAllRanges();
+		var x = e.pageX - $('#color-palette').offset().left;
+		if(!(r.index() == 0)) {
+			var prev = r.prev();
+            if(r.position().top == prev.position().top) {
+                if(x < (prev.position().left - (prev.width()))) {
+                    r.insertBefore(prev);
+                }
+            }
+		}
+
+		if(!(r.index() == $('.color').length - 1)) {
+			var next = r.next();
+            if(r.position().top == next.position().top) {
+                if(x > (next.position().left - (next.width()))) {
+                    r.insertAfter(next);
+                }
+            }
+		}
+
+        var y = e.pageY;
+        if(!(r.index == ($('.color').length - 1))) {
+            if(y > r.position().top + (r.height()) * 1.5) {
+                var next = r.next();
+                while(next.position().left != r.position().left ) {
+                    if(next.index() == ($('.color').length - 1)) {
+                        r.insertAfter(next);
+                        return;
+                    }
+                    next = next.next();
+                }
+                r.insertAfter(next);
+            }
+        }
+
+        if(!(r.index == 0)) {
+            if(y < r.position().top - (r.height())) {
+                var prev = r.prev();
+                while(prev.position().left != r.position().left ) {
+                    if(prev.index() == 0) {
+                        r.insertBefore(prev);
+                        return;
+                    }
+                    prev = prev.prev();
+                }
+                r.insertBefore(prev);
+            }
+        }
+    }
+});
 
 $('#addToPalette').on('click', function(e) {
     addPaletteItem(currTool.color);
 });
+
+
 
 //TODO: combine these 2 functions?
 function initSize(id) {

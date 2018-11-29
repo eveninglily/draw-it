1bb47e9d33cfdf398200e5e8e9b9f20be6a0e9cb
Evan McIntire <mcintire.evan@gmail.com>
2018-11-29 01:29:42 -0500

    Finish basic function of color picker

--- src/client/draw/DrawingApp.tsx
+++ src/client/draw/DrawingApp.tsx
@@ -3,20 +3,25 @@ import RCanvas from 'client/draw/canvas/RCanvas';
 import Client from 'client/draw/Client';
 import ColorPicker from 'client/draw/components/ColorPicker';
 import Toolbar from 'client/draw/ui/Toolbar';
+import * as Color from 'color';
 import * as React from 'react';
 
 interface DrawingAppState {
   layers: any[];
   selectedTool: string;
   client: Client;
+  color: Color;
 }
 
 class DrawingApp extends React.Component<{}, DrawingAppState> {
   constructor(props: any) {
     super(props);
 
+    const color = new Color({r: 255, b: 0, g: 0});
+
     this.state = {
-      client: new Client('localhost:3001'),
+      client: new Client('draw-it.io:3001'),
+      color,
       layers: [new ExCanvas()],
       selectedTool: 'brush',
     }
@@ -32,12 +37,19 @@ class DrawingApp extends React.Component<{}, DrawingAppState> {
     });
   }
 
+  public onColorChange = (color: Color) => {
+    this.setState({color});
+  }
+
   public render() {
+    const st = {
+      background: this.state.color.hex(),
+    }
     return (
-      <div id="container">
+      <div id="container" style={st}>
         <Toolbar selectedTool={this.state.selectedTool} updateTool={this.selectTool}/>
         <div id='colorwheel'>
-          <ColorPicker/>
+          <ColorPicker onColorChange={this.onColorChange}/>
         </div>
         <div id='tool-meta' />
         <div id='layer-options' />

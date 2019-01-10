8b0dd7ce26c1525b3303c84bc18d5cd621c280d4
Evan McIntire <mcintire.evan@gmail.com>
2019-01-10 11:59:53 -0500

    Implement modal

--- src/client/components/Modal.tsx
+++ src/client/components/Modal.tsx
@@ -1,19 +1,18 @@
+import 'client/css/Modal.css'
 import * as React from 'react';
 
-interface ModalState {
+interface ModalProps {
   isVisible: boolean;
+  onBackgroundClick: any;
 }
 
-class Modal extends React.Component<{}, ModalState> {
-  public dismiss(): void {
-    this.setState({
-      isVisible: false,
-    })
-  }
-
+class Modal extends React.Component<ModalProps, {}> {
   public render() {
+    if(!this.props.isVisible) {
+      return null;
+    }
     return (
-      <div className='modalBackground' onClick={this.dismiss}>
+      <div className='modalBackground' onClick={this.props.onBackgroundClick}>
         <div className='modal'>{this.props.children}</div>;
       </div>
     );
